011801a9e2915dca58578def805e3c35a50a093a
vaknin <vaknins33@gmail.com>
2019-07-29 08:56:40 +0300

    grid and axes are drawn

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Global Variables
+// Canvas initialization
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Draw the grid on canvas
 function drawGrid(){
     let img = new Image(500,500);
     img.src = './grid.svg';
@@ -12,4 +13,29 @@ function drawGrid(){
     }
 }
 
+// Draw the axes
+function drawAxes(){
+
+    // Axes line properties
+    ctx.lineWidth = 2;
+
+    // Y-axis
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+    ctx.closePath();
+
+    // X-axis
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height / 2);
+    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.stroke();
+    ctx.closePath();
+
+    // Revert axes line properties
+    ctx.lineWidth = 1;
+}
+
+drawAxes();
 drawGrid();
